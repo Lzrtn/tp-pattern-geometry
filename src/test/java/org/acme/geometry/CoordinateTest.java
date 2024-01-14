a294c2bbfa4dc6758e08eb5d01869983c7064edf
L_zrtn <lisa.zaratin@ensg.eu>
2024-01-14 11:42:03 +0100

    Question0.1

--- src/test/java/org/acme/geometry/CoordinateTest.java
+++ src/test/java/org/acme/geometry/CoordinateTest.java
@@ -9,10 +9,26 @@ public class CoordinateTest {
 
 	@Test
 	public void testConstructorXY(){
-		// TODO
-		//Coordinate c = new Coordinate(3.0,4.0);
-		//Assert.assertEquals(3.0, c.getX(), EPSILON);
-		//Assert.assertEquals(4.0, c.getY(), EPSILON);
+		Coordinate c = new Coordinate(3.0,4.0);
+		Assert.assertEquals(3.0, c.getX(),EPSILON);
+		Assert.assertEquals(4.0, c.getY(), EPSILON);
 	}
+	
+	@Test
+	public void testEmpty() {
+		Coordinate c = new Coordinate();
+		Assert.assertTrue(Double.isNaN(c.getX()));
+		Assert.assertTrue(Double.isNaN(c.getY()));
+		Assert.assertTrue(c.isEmpty());
+	}
+	
+	@Test
+	public void testString() {
+		Coordinate c = new Coordinate(4.0,4.0);
+		Assert.assertEquals("[4.0,4.0]", c.toString());
+		Coordinate n = new Coordinate();
+		Assert.assertEquals("[NaN,NaN]",n.toString());
+	}
+	
 
 }
